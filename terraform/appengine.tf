cf4c022179a013dc58727d4fd99d638fc145cac3
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2023-07-15 10:25:54 -0400

    tf test
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>

--- terraform/appengine.tf
+++ terraform/appengine.tf
@@ -8,10 +8,19 @@ resource "google_app_engine_standard_app_version" "gae-appengine-example" {
     service         = "gae-appengine-example"
     runtime         = "java17"
     app_engine_apis = true
-    location_id = "us-east1"
     service_account = google_service_account.gae_service_account.email
 
     env_variables = {
         port = "8080"
+    }
+
+    entrypoint {
+        shell = "java -Xmx128m -jar gcp-appengine-example.jar"
+    }
+
+    deployment {
+        zip {
+            source_url = "https://storage.googleapis.com/dotcomrow-gcp-appengine-example/gcp-appengine-example.jar"
+        }
     }
 }
