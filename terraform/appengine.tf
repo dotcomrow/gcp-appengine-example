b2784ba21a1e4a5a7035ffa7c4bcd3e2e503eca2
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2023-07-15 10:13:32 -0400

    update
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>

---
+++ terraform/appengine.tf
@@ -0,0 +1,18 @@
+resource "google_service_account" "gae_service_account" {
+  account_id   = "gae-service-account"
+  display_name = "GAE Service Account"
+}
+
+resource "google_app_engine_application" "app" {
+    version_id      = "v2"
+    service         = "gae-appengine-example"
+    runtime         = "java17"
+    app_engine_apis = true
+    project     = <GCP_PROJECT_ID>
+    location_id = "us-east1"
+    service_account = google_service_account.gae_service_account.email
+
+    env_variables = {
+        port = "8080"
+    }
+}
